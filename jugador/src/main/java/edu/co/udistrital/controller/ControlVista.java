d696e22299c8bd65dace5817a5398039059d8d0f
karinaroa0207 <akroam@udistrital.edu.co>
2026-04-27 11:30:00 -0500

    actualizado

--- jugador/src/main/java/edu/co/udistrital/controller/ControlVista.java
+++ jugador/src/main/java/edu/co/udistrital/controller/ControlVista.java
@@ -5,39 +5,69 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- *
- * @author mauri
+ * Controla la comunicación entre la vista y la lógica del juego.
  */
-public class ControlVista implements ActionListener{
+public class ControlVista implements ActionListener {
+
     private JuegoView vista;
     private JuegoController juego;
 
+    /**
+     * Inicializa la vista y el controlador del juego.
+     */
     public ControlVista(JuegoController juego) {
-        vista = new JuegoView(this);
+        this.vista = new JuegoView(this);
         this.juego = juego;
-    }   
-    
-    public void pedirNumeroJugadores() {        
+    }
+
+    /**
+     * Solicita el número de jugadores, inicia la ventana
+     * y ejecuta el juego con ese valor.
+     */
+    public void pedirNumeroJugadores() {
         int n = vista.pedirNumeroJugadores();
         vista.iniciarVentana(n);
-        juego.ejecutarJuego(n);       
+        juego.ejecutarJuego(n);
     }
-       
+
+    /**
+     * Muestra en la vista el resultado del lanzamiento de un jugador.
+     * 
+     * @param idJugador identificador del jugador
+     * @param valorDado valor obtenido en el dado
+     */
     public void mostrarLanzamiento(int idJugador, int valorDado) {
         vista.mostrarJugadorLanza(idJugador, valorDado);
     }
-    
+
+    /**
+     * Muestra si un jugador sigue activo o fue eliminado,
+     * junto con el valor obtenido en el dado.
+     * 
+     * @param idJugador identificador del jugador
+     * @param eliminado estado del jugador
+     * @param valorDado valor del dado
+     */
     public void mostrarEstadoJugador(int idJugador, boolean eliminado, int valorDado) {
         vista.mostrarEstadoJugador(idJugador, eliminado, valorDado);
     }
-    
+
+    /**
+     * Muestra el jugador ganador en la vista.
+     * 
+     * @param idJugador identificador del ganador
+     */
     public void mostrarGanador(int idJugador) {
         vista.mostrarGanador(idJugador);
     }
 
+    /**
+     * Maneja acciones de la interfaz como reiniciar o salir.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         String cmd = e.getActionCommand();
+
         if (cmd.equalsIgnoreCase("reiniciar")) {
             vista.reiniciar();
             juego.reiniciar();
